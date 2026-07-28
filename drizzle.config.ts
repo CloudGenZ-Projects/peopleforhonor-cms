e88470daf14baad2511204bfe7562aef935a75de
Mayur <cloudgenz.skillsquirrel@gmail.com>
2026-07-28 13:16:55 +0530

    Set schema path in drizzle.config.ts for db:push

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'drizzle-kit'
 import { config } from 'dotenv'
 
-// Load .env or .env.production or .env.local automatically
 config({ path: '.env' })
 config({ path: '.env.production' })
 config({ path: '.env.local' })
 
 export default defineConfig({
   dialect: 'postgresql',
+  schema: './src/payload-types.ts',
   dbCredentials: {
     url: process.env.DATABASE_URI || '',
   },
