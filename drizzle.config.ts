08768ce91f6e6ed7e250ee2ea2c9457d81099fdc
Mayur <cloudgenz.skillsquirrel@gmail.com>
2026-07-28 12:43:06 +0530

    Support .env, .env.production and .env.local for Drizzle Studio

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,10 @@
 import { defineConfig } from 'drizzle-kit'
+import { config } from 'dotenv'
+
+// Load .env or .env.production or .env.local automatically
+config({ path: '.env' })
+config({ path: '.env.production' })
+config({ path: '.env.local' })
 
 export default defineConfig({
   dialect: 'postgresql',
